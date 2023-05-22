3358d08ab20d0cb7c3cc80ff8bcdbbfe7fedb76d
tmco28 <tranmongco06@gmail.com>
2023-05-22 08:50:37 +0700

    add new project lan1

---
+++ gitcoban/src/main/java/com/fpoly/GitcobanApplication.java
@@ -0,0 +1,13 @@
+package com.fpoly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcobanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitcobanApplication.class, args);
+	}
+
+}
